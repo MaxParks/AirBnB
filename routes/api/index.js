13ca7f5aa2b20ef624d272eda2a027e28799a64a
Max Parks <novarecon@yahoo.com>
2023-02-08 18:01:20 -0800

    Last windows update for this

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,19 @@ const router = require('express').Router();
 // backend/routes/api/index.js
 // ...
 
+// GET /api/set-token-cookie
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user });
+});
+
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
