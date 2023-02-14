133fe71bd9c1fd9c024275b08acbe6e5357d5c75
MaxParks <novarecon@yahoo.com>
2023-02-14 06:04:20 -0800

    fixed signup

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -67,16 +67,10 @@ module.exports = (sequelize, DataTypes) => {
       firstName: {
         type: DataTypes.STRING,
         allowNull: false,
-        validate: {
-          len: [20]
-        }
       },
       lastName: {
         type: DataTypes.STRING,
         allowNull: false,
-        validate: {
-          len: [20]
-        }
       },
       email: {
         type: DataTypes.STRING,
