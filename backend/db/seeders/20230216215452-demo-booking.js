0953d2e57cf068979f4a0114121ec13758d788da
MaxParks <novarecon@yahoo.com>
2023-02-19 12:49:59 -0800

    changed some stuff

--- backend/db/seeders/20230216215452-demo-booking.js
+++ backend/db/seeders/20230216215452-demo-booking.js
@@ -43,7 +43,7 @@ module.exports = {
     options.tableName = 'Bookings';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: [1] }
+      spotId: { [Op.in]: [1, 2, 3] }
     }, {});
   }
 };
