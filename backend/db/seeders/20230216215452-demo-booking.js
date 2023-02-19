2c4417e8bc6ad01bcbe4720adb8a826487a7b10f
MaxParks <novarecon@yahoo.com>
2023-02-19 13:39:58 -0800

    hopeful fix

--- backend/db/seeders/20230216215452-demo-booking.js
+++ backend/db/seeders/20230216215452-demo-booking.js
@@ -1,6 +1,5 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
@@ -11,22 +10,23 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         spotId: 1,
-        userId: 1,
+        userId: 2,
         startDate: "2023-10-01",
         endDate: "2023-11-16"
       },
       {
         spotId: 2,
-        userId: 2,
+        userId: 3,
         startDate: "2023-11-01",
         endDate: "2023-12-16"
       },
       {
         spotId: 3,
-        userId: 3,
+        userId: 1,
         startDate: "2023-10-01",
         endDate: "2023-12-16"
       },
+      //hi
     /**
      * Add seed commands here.
      *
