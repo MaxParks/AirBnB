b10d44a4d0657d6a9e8362c65a86f38c79fac61e
MaxParks <novarecon@yahoo.com>
2023-02-22 14:43:46 -0800

    fixing date error stuff

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -25,11 +25,11 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     startDate: {
-      type: DataTypes.DATEONLY,
+      type: DataTypes.DATE,
       allowNull: false
     },
     endDate: {
-      type: DataTypes.DATEONLY,
+      type: DataTypes.DATE,
       allowNull: false
     },
   }, {
