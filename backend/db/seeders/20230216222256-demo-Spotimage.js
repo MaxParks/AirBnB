b685f777d8f8ee1d07b5dad48407f14eb4624727
Max J Parks <109090029+MaxParks@users.noreply.github.com>
2023-04-15 12:13:22 -0700

    Add files via upload

--- backend/db/seeders/20230216222256-demo-Spotimage.js
+++ backend/db/seeders/20230216222256-demo-Spotimage.js
@@ -11,17 +11,17 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         spotId: 1,
-        url: 'www.google.com/1',
+        url: 'https://www.bhg.com/thmb/H9VV9JNnKl-H1faFXnPlQfNprYw=/1799x0/filters:no_upscale():strip_icc()/white-modern-house-curved-patio-archway-c0a4a3b3-aa51b24d14d0464ea15d36e05aa85ac9.jpg',
         preview: true,
       },
       {
         spotId: 2,
-        url: 'www.google.com/2',
+        url: 'https://ap.rdcpix.com/399e65db076d5e548462ebdc27fed591l-m493598138od-w480_h360_x2.jpg',
         preview: true,
       },
       {
         spotId: 3,
-        url: 'www.google.com/3',
+        url: 'https://cdn2.lamag.com/wp-content/uploads/sites/6/2018/06/house-los-angeles-getty.jpg',
         preview: true,
       },
     /**
@@ -40,7 +40,7 @@ module.exports = {
     options.tableName = 'Spotimages';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      url: { [Op.in]: ['www.google.com/1','www.google.com/2','www.google.com/3'] }
+      url: { [Op.in]: ['https://www.bhg.com/thmb/H9VV9JNnKl-H1faFXnPlQfNprYw=/1799x0/filters:no_upscale():strip_icc()/white-modern-house-curved-patio-archway-c0a4a3b3-aa51b24d14d0464ea15d36e05aa85ac9.jpg','https://ap.rdcpix.com/399e65db076d5e548462ebdc27fed591l-m493598138od-w480_h360_x2.jpg','https://cdn2.lamag.com/wp-content/uploads/sites/6/2018/06/house-los-angeles-getty.jpg'] }
     }, {});
   }
 };
